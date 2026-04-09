88fe4388eba0078bf1241312f2f674c101b2c265
Merve Gül ATALAY <mervegulatly7@gmail.com>
2026-04-09 23:57:54 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+courses_db = [
+    {'id': 1, 'instructor': 'Merve', 'title': 'Python', 'category': 'Development'},
+    {'id': 2, 'instructor': 'Ahmet', 'title': 'Java', 'category': 'Development'},
+    {'id': 3, 'instructor': 'Fatma', 'title': 'Jenkins', 'category': 'Devops'},
+    {'id': 4, 'instructor': 'Zeynep', 'title': 'Kubernetes', 'category': 'Devops'},
+    {'id': 5, 'instructor': 'Merve', 'title': 'Machine Learning', 'category': 'AI'},
+    {'id': 6, 'instructor': 'Ali', 'title': 'Deep Learning', 'category': 'AI'},
+]
+
+@app.get("/hello")
+async def hello_world():
+    return {"message": "Hello World!"}
+
+
+@app.get("/courses")
+async def get_all_courses():
+    return courses_db
+
+#Path Parameter
+@app.get("/courses/{courses_title}")
+async def get_course(courses_title: str):
+    for course in courses_db:
+        if course['title'].casefold() == courses_title.casefold():
+            return course
+
+
+#This function cannot work because they point to the same path.
+@app.get("/courses/{courses_id}")
+async def get_course_by_id(courses_id: int):
+    for course in courses_db:
+        if course.get('id') == courses_id:
+            return course
+
+
+@app.get("/course/byid/{courses_id}")
+async def get_course_by_id(courses_id: int):
+    for course in courses_db:
+        if course.get('id') == courses_id:
+            return course
